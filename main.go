0f0f38f19dce8e17c7c3da0d6dca826ff65adcd9
J1nAB1n9 <415086234@qq.com>
2022-12-11 00:24:23 +0800

    http example

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package mhttpsvr
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+// fmt.Fprintf 根据格式对io write写入
+func HelloMsg(w http.ResponseWriter ,r *http.Request)  {
+	fmt.Fprintf(w,"Http %s\n",r.URL.Query().Get("name"))
+}
 
 func main() {
 	fmt.Println("Hello World")
+	http.HandleFunc("/hello",HelloMsg)
+	http.ListenAndServe("19000",nil)
 }
