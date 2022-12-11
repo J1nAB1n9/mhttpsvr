e4fa04c14fce33f597a8a157e6690543a39daf66
J1nAB1n9 <415086234@qq.com>
2022-12-11 20:39:23 +0800

    bug修复

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package mhttpsvr
+package main
 
 import (
 	"fmt"
@@ -7,11 +7,15 @@ import (
 
 // fmt.Fprintf 根据格式对io write写入
 func HelloMsg(w http.ResponseWriter ,r *http.Request)  {
+	// get http://127.0.0.1:8000/hello?name=me
 	fmt.Fprintf(w,"Http %s\n",r.URL.Query().Get("name"))
 }
 
 func main() {
 	fmt.Println("Hello World")
 	http.HandleFunc("/hello",HelloMsg)
-	http.ListenAndServe("19000",nil)
+	err := http.ListenAndServe(":8000",nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
